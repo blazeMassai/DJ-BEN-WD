1d6a1c667caa029000a2b52fdc6c866db3ad239c
Reward I Masalla <reward.masalla@trc.co.tz>
2022-08-15 15:55:23 +0300

    kurasa 185

--- msomi/reviews/views.py
+++ msomi/reviews/views.py
@@ -1,8 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
+from .utils import average_rating
 
 
 # Create your views here.
 def index(request):
-    name="Johnny"
-    return render(request, 'base.html', {"name":name} )
+    return render(request, 'base.html')
+
+
+def book_list(request):
+    books = Book.objects.all()
+    books_with_reviews = []
+    for book in books:
+        reviews = book.review_set.all()
+        if reviews:
+            book_rating = average_rating([review.rating for review in reviews])
+            number_of_reviews = len(reviews)
+        else:
+            book_rating = None
+            number_of_reviews
