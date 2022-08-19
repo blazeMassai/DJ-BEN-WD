701ca82f7e57d246c3f3303e0cbed0f47550ec58
Reward I Masalla <reward.masalla@trc.co.tz>
2022-08-19 16:34:38 +0300

    19-8-2022

--- msomi/msomi/settings.py
+++ msomi/msomi/settings.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.humanize' ,
+    'django.contrib.humanize',
     'reviews',
 ]
 
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'msomi.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -82,10 +79,9 @@ DATABASES = {
         'USER': 'postgres',
         'PASSWORD': 'anungo710',
         'HOST': 'localhost',
-        'PORT': '5432' ,
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -105,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -117,7 +112,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
@@ -127,3 +121,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
--- msomi/reviews/admin.py
+++ msomi/reviews/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from reviews.models import (Publisher, Contributor, Book,
+                            BookContributor, Review)
+from django.contrib.admin import AdminSite
+
+class BookAdminSite(AdminSite):
+    title_header = 'Msomi'
+    site_header = 'Msomi Administrator'
+    index_title = 'Msomi Site Admin'
+
+class BookAdmin(admin.ModelAdmin):
+    date_hierarchy = 'publication_date'
+    list_display = ('id', 'title', 'isbn')
+    list_filter = ('publisher', 'publication_date')
+
+
+def initialled_name(obj):
+    """ obj.first_names='Jerome David', obj.last_names='Salinger'
+        => 'Salinger, JD' """
+    initials = ''.join([name[0] for name in obj.first_names.split(' ')])
+    return "{}, {}".format(obj.last_names, initials)
+
+
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'last_names', 'first_names')
+    list_filter = ('last_names',)
+    search_fields = ('last_names__startswith', 'first_names')
+
 
 # Register your models here.
+admin.site.register(Publisher)
+admin.site.register(Contributor, ContributorAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(BookContributor)
+admin.site.register(Review)
+admin_site = BookAdminSite(name='Msomi')
